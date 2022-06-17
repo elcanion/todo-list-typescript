d5d6324de3159e4b42f7a5e2393c43848387da16
elcanion <elcaniondev@gmail.com>
2022-06-17 00:12:22 -0300

    commit

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,14 +1,19 @@
 import * as C from "./styles";
 import { Item } from "../../types/Item";
+import { useState } from "react";
 
 type Props = {
     item: Item
 }
 
 export const ListItem = ({ item }: Props) => {
+
+    const [isChecked, setIsChecked] = useState(item.isDone);
+
     return (
         <C.Container>
-            {item.name}
+            <input type="checkbox" />
+            <label>{item.name}</label>
         </C.Container>
     );
 }
