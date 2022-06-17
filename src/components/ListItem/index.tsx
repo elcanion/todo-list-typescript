ff740dd6a38144a81de8f4f7e6eaa48fd3762494
elcanion <elcaniondev@gmail.com>
2022-06-17 00:17:07 -0300

    styled function

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -11,8 +11,12 @@ export const ListItem = ({ item }: Props) => {
     const [isChecked, setIsChecked] = useState(item.isDone);
 
     return (
-        <C.Container>
-            <input type="checkbox" />
+        <C.Container isDone={isChecked}>
+            <input 
+                type="checkbox" 
+                checked={isChecked}
+                onChange={e => setIsChecked(e.target.checked)}    
+            />
             <label>{item.name}</label>
         </C.Container>
     );
